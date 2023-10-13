35854b4cc87e7ebebf4ea67cdad5058c8edfcb52
Sthefany Silva <sthefany0011@gmail.com>
2023-10-12 23:31:20 -0300

    add endpoint perfilUsuario

--- src/rotas.js
+++ src/rotas.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { cadastrarUsuario, login} = require('./controladores/usuarios');
+const { cadastrarUsuario,perfilUsuario, login} = require('./controladores/usuarios');
 const autenticaLogin = require('./intermediario/autenticaLogin');
 const { listarCategorias } = require('./controladores/categorias');
 
 const rotas = express();
 
 rotas.post('/usuario', cadastrarUsuario);
+rotas.get('/usuario', perfilUsuario);
 rotas.post('/login', login);
 
 rotas.get('/categoria', listarCategorias);
