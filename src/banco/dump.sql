067d91520a6344fefaa6065af204f780deba4979
Leila.bwt <leila.bwt@gmail.com>
2023-10-18 16:12:19 -0300

    Inserção tabelas produtos e clientes

--- src/banco/dump.sql
+++ src/banco/dump.sql
@@ -37,3 +37,25 @@ VALUES
 ('Moda'),
 ('Bebê'),
 ('Games'),
+
+
+create table produtos(
+    id SERIAL PRIMARY KEY,
+    descricao VARCHAR(255) NOT NULL,
+    quantidade_estoque INT NOT NULL,
+    valor INT NOT NULL,
+    categoria_id INT NOT NULL,
+);
+
+create table clientes(
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    cpf VARCHAR(14) UNIQUE NOT NULL,
+    cep VARCHAR(20) NOT NULL,
+    rua VARCHAR(255) NOT NULL,
+    numero VARCHAR(10) NOT NULL,
+    bairro VARCHAR(50) NOT NULL,
+    cidade VARCHAR(50) NOT NULL,
+    estado VARCHAR(50) NOT NULL,
+);
