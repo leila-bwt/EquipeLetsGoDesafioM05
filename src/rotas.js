21283c30d1d0debbcbf9fcb0eab55e890443a2c2
Stephanie-Feliciano <stephaniesantanafeliciano@hotmail.com>
2023-10-20 20:11:12 -0300

    Criação da rota listar-Produtos concluída

--- src/rotas.js
+++ src/rotas.js
@@ -1,26 +1,37 @@
-const express = require('express');
+const express = require("express");
 
-const { cadastrarUsuario, login, editarUsuario, detalharUsuario } = require('./controladores/usuarios');
+const {
+  cadastrarUsuario,
+  login,
+  editarUsuario,
+  detalharUsuario,
+} = require("./controladores/usuarios");
 
-const autenticaLogin = require('./intermediario/autenticaLogin');
-const { listarCategorias } = require('./controladores/categorias');
-const { cadastrarProdutos, editarProduto, obterProdutoId } = require('./controladores/produtos');
+const autenticaLogin = require("./intermediario/autenticaLogin");
+const { listarCategorias } = require("./controladores/categorias");
+const {
+  cadastrarProdutos,
+  editarProduto,
+  obterProdutoId,
+} = require("./controladores/produtos");
+const { listarProdutos } = require("./controladores/listarProdutos");
 
 const rotas = express();
 
-rotas.get('/categoria', listarCategorias);
+rotas.get("/categoria", listarCategorias);
 
-rotas.post('/usuario', cadastrarUsuario);
+rotas.post("/usuario", cadastrarUsuario);
 
-rotas.post('/login', login);
+rotas.post("/login", login);
 rotas.use(autenticaLogin);
 
-rotas.get('/usuario', detalharUsuario);
-rotas.put('/usuario', editarUsuario);
+rotas.get("/usuario", detalharUsuario);
+rotas.put("/usuario", editarUsuario);
 
-rotas.post('/produtos', cadastrarProdutos);
-rotas.put('/produto/:id', editarProduto)
+rotas.post("/produtos", cadastrarProdutos);
+rotas.put("/produto/:id", editarProduto);
 
-rotas.get('/produto/:id', obterProdutoId)
+rotas.get("/produto/:id", obterProdutoId);
+rotas.get("/produto", listarProdutos);
 
 module.exports = rotas;
