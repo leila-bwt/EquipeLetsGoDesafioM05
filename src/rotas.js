f3f99a9da008a69fee926d18bd7082ab1b6aa8c8
Stephanie-Feliciano <stephaniesantanafeliciano@hotmail.com>
2023-10-25 20:44:04 -0300

    Criação da rota ListarPedido

--- src/rotas.js
+++ src/rotas.js
@@ -1,20 +1,31 @@
 const express = require("express");
 
-const {cadastrarUsuario,login} = require("./controladores/usuarios");
+const { cadastrarUsuario, login } = require("./controladores/usuarios");
 const { listarCategorias } = require("./controladores/categorias");
 
 const autenticaLogin = require("./intermediario/autenticaLogin");
 
-const {  editarUsuario, detalharUsuario} = require ("./controladores/usuarios");
+const { editarUsuario, detalharUsuario } = require("./controladores/usuarios");
 
+const {
+  cadastrarCliente,
+  editarCliente,
+  listarClientes,
+  detalharClienteId,
+} = require("./controladores/clientes");
 
-const { cadastrarCliente,
-       editarCliente,
-       listarClientes,
-       detalharClienteId } = require('./controladores/clientes');
-
-const {cadastrarProdutos,editarProduto,obterProdutoId,listarProdutos, excluirProdutoPorId} = require("./controladores/produtos");
-const {cadastrarPedido} = require("./controladores/pedidos");
+const {
+  cadastrarProdutos,
+  editarProduto,
+  obterProdutoId,
+  listarProdutos,
+  excluirProdutoPorId,
+} = require("./controladores/produtos");
+const { cadastrarPedido } = require("./controladores/pedidos");
+const {
+  listarPedidos,
+  listarPedido,
+} = require("./controladores/listarPedidos");
 
 const rotas = express();
 
@@ -28,22 +39,21 @@ rotas.use(autenticaLogin);
 rotas.get("/usuario", detalharUsuario);
 rotas.put("/usuario", editarUsuario);
 
-
 rotas.post("/produto", cadastrarProdutos);
 rotas.put("/produto/:id", editarProduto);
 
 rotas.get("/produto/:id", obterProdutoId);
 rotas.get("/produto", listarProdutos);
 
-rotas.delete('/produto/:id', excluirProdutoPorId)
+rotas.delete("/produto/:id", excluirProdutoPorId);
 
-rotas.post('/cliente', cadastrarCliente);
-rotas.put('/cliente/:id', editarCliente);
+rotas.post("/cliente", cadastrarCliente);
+rotas.put("/cliente/:id", editarCliente);
 
-rotas.get('/cliente/', listarClientes)
-rotas.get('/cliente/:id', detalharClienteId)
+rotas.get("/cliente/", listarClientes);
+rotas.get("/cliente/:id", detalharClienteId);
 
-rotas.post('/pedido', cadastrarPedido);
-
+rotas.post("/pedido", cadastrarPedido);
+rotas.get("/pedido", listarPedido);
 
 module.exports = rotas;
